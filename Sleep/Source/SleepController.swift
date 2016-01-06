0c0d315c9695e7a3a5223645aee421dcaebd55e3
Julian Grosshauser <julian@juliangrosshauser.com>
2016-01-06 15:52:59 +0100

    Add SleepController class

---
+++ Sleep/Source/SleepController.swift
@@ -0,0 +1,11 @@
+//
+//  SleepController.swift
+//  Sleep
+//
+//  Created by Julian Grosshauser on 06/01/16.
+//  Copyright © 2016 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+final class SleepController: UIViewController {}
